9cb7a4e56502f3670d60bca605178f344b1bedeb
Administrator <Administrator@IMVN1S715YUHN9O>
2017-11-02 17:19:34 +0800

    批量插入

--- src/main/java/demo/Demo1.java
+++ src/main/java/demo/Demo1.java
@@ -2,6 +2,7 @@ package demo;
 
 import java.io.IOException;
 import java.io.InputStream;
+import java.util.ArrayList;
 import java.util.List;
 
 import org.apache.ibatis.io.Resources;
@@ -19,28 +20,47 @@ public class Demo1 {
 		SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(is);
 		SqlSession session = sf.openSession();
 		try{
+			//主键查找
 //			String str = "dao.UserMapper.findById";
 //			User user = session.selectOne(str, 1L);
 //			System.out.println(user.toString());
 			
+			//批量插入
+			String batchInsert = "dao.UserMapper.batchInsert";
+			List<User> insertList = new ArrayList<User>();
+			for(int i = 0; i< 5 ; i++){
+				User u  = new User();
+				u.setAge(i*10L);
+				u.setName(String.valueOf(i*i*i));
+				u.setRealName(String.valueOf('a'+i));
+				u.setDes("第几个:"+i);
+				insertList.add(u);
+			}
+			int insert = session.insert(batchInsert, insertList);
+			System.out.println(insert);
+			session.commit();
+			
+			//查询所有
 			String str1 = "dao.UserMapper.findAll";
 			List<User> list = session.selectList(str1);
 			for (User u : list) {
 				System.out.println(u.toString());
 			}
 			
-			String str2 = "dao.UserMapper.insertUser";
-			User i = new User();
-			i.setName("dd");
-			i.setRealName("大大");
-			i.setAge(10L);
-			i.setDes("VC可不好粗预告");
-			int insert = session.insert(str2, i);
-			session.commit();
-			System.out.println(insert);
-			String str3 = "dao.UserMapper.findByName";
-			List<User> selectList = session.selectList(str3, "dd");
-			System.out.println(selectList.size());
+			//单一插入
+//			String str2 = "dao.UserMapper.insertUser";
+//			User i = new User();
+//			i.setName("dd");
+//			i.setRealName("大大");
+//			i.setAge(10L);
+//			i.setDes("VC可不好粗预告");
+//			int insert = session.insert(str2, i);
+//			System.out.println(insert);
+//			session.commit();
+			//equal查询
+//			String str3 = "dao.UserMapper.findByName";
+//			List<User> selectList = session.selectList(str3, "dd");
+//			System.out.println(selectList.size());
 			
 		}finally{
 			session.close();
