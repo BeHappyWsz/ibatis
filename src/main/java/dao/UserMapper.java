2c606a2afd7e61398d3388641dcd635f4452e133
BeHappyWsz <wang865744553>
2017-11-02 20:21:19 +0800

    123

--- src/main/java/dao/UserMapper.java
+++ src/main/java/dao/UserMapper.java
@@ -2,6 +2,18 @@ package dao;
 
 import domain.User;
 
+import java.util.List;
+
 public interface UserMapper {
+
 	public User findById(Long id);
+
+	public List<User> findAll();
+
+	public int insertUser(User user);
+
+	public void batchInsert(List<User> list);
+
+	public  User findByName(String name);
+
 }
--- src/main/java/demo/Demo1.java
+++ src/main/java/demo/Demo1.java
@@ -20,25 +20,25 @@ public class Demo1 {
 		SqlSessionFactory sf = new SqlSessionFactoryBuilder().build(is);
 		SqlSession session = sf.openSession();
 		try{
-			//主键查找
+//			//主键查找
 //			String str = "dao.UserMapper.findById";
 //			User user = session.selectOne(str, 1L);
 //			System.out.println(user.toString());
 			
 			//批量插入
-			String batchInsert = "dao.UserMapper.batchInsert";
-			List<User> insertList = new ArrayList<User>();
-			for(int i = 0; i< 5 ; i++){
-				User u  = new User();
-				u.setAge(i*10L);
-				u.setName(String.valueOf(i*i*i));
-				u.setRealName(String.valueOf('a'+i));
-				u.setDes("第几个:"+i);
-				insertList.add(u);
-			}
-			int insert = session.insert(batchInsert, insertList);
-			System.out.println(insert);
-			session.commit();
+//			String batchInsert = "dao.UserMapper.batchInsert";
+//			List<User> insertList = new ArrayList<User>();
+//			for(int i = 0; i< 5 ; i++){
+//				User u  = new User();
+//				u.setAge(i*10L);
+//				u.setName(String.valueOf(i*i*i));
+//				u.setRealName(String.valueOf('a'+i));
+//				u.setDes("第几个:"+i);
+//				insertList.add(u);
+//			}
+//			int insert = session.insert(batchInsert, insertList);
+//			System.out.println(insert);
+//			session.commit();
 			
 			//查询所有
 			String str1 = "dao.UserMapper.findAll";
@@ -61,7 +61,6 @@ public class Demo1 {
 //			String str3 = "dao.UserMapper.findByName";
 //			List<User> selectList = session.selectList(str3, "dd");
 //			System.out.println(selectList.size());
-			
 		}finally{
 			session.close();
 		}
